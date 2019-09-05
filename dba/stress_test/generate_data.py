e7828eb4832dc2e162e6448868b4fc3aa8ec1cc1
qixiangyang <qixiangyangrm@foxmail.com>
2019-09-05 15:18:26 +0800

    PostgreSQL 压力测试
    目前完成
    1. 测试数据的生成
    2. PostgreSQL 数据插入的代码测试
    3. 插入计时

---
+++ dba/stress_test/generate_data.py
@@ -0,0 +1,25 @@
+"""
+Description:
+Author:qxy
+Date: 2019-09-05 13:49
+File: generate_data 
+"""
+
+
+def gen_test_data(x):
+    test_data = []
+    base_data = {
+        'title': 'title' * 10,
+        'text': 'text' * 1000,
+        'date': '2019-08-28'
+    }
+    for _ in range(x):
+        base_data['index'] = _
+        test_data.append(base_data)
+    return test_data
+
+
+if __name__ == '__main__':
+    data = gen_test_data(10)
+    print(data)
+    # print(len(data))
--- dba/stress_test/postgre.py
+++ dba/stress_test/postgre.py
@@ -5,3 +5,56 @@ Date: 2019-09-03 18:12
 File: postgre 
 """
 
+import psycopg2
+import time
+from generate_data import gen_test_data
+
+
+conn = psycopg2.connect(database="wangyuebing", user="postgres",
+                        password="12345678", host="127.0.0.1",
+                        port="5432")
+
+print("Opened database successfully")
+
+cur = conn.cursor()
+
+# cur.execute("""create table article_info (
+#             id serial primary key,
+#             title text not null,
+#             text text not null,
+#             date date not null)""")
+# print('创建表成功')
+
+# cur.execute("insert into article_info (title, text, date) values ('%s', '%s', '%s')" % ('ssss', 'sssssssss', '2019-08-02'))
+# conn.commit()
+# conn.close()
+
+
+def insert_data(x):
+
+    test_data_list = gen_test_data(x)
+    count = 1
+    count_time = time.time()
+
+    for test_data in test_data_list:
+        sql = "insert into article_info (title, text, date) values " \
+              "('%s', '%s', '%s')" % (str(test_data['title']), test_data['text'], test_data['date'])
+
+        cur.execute(sql)
+        count += 1
+        if count % 1000 == 0:
+            conn.commit()
+            tmp_time = time.time()
+            per_time = tmp_time - count_time
+            print(count, per_time)
+            count_time = time.time()
+
+    conn.close()
+
+
+if __name__ == '__main__':
+
+    time_start = time.time()
+    insert_data(1000000)
+    time_end = time.time()
+    print('totally cost', time_end - time_start)

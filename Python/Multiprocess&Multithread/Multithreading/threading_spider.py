2a7d5917bf39a9969bf8faeac1e7516f3405e331
qixiangyang <qixiangyangrm@foxmail.com>
2019-11-27 14:59:08 +0800

    10个线程 对应10个分片好的任务

--- Python/Multiprocess&Multithread/Multithreading/threading_spider.py
+++ Python/Multiprocess&Multithread/Multithreading/threading_spider.py
@@ -16,28 +16,33 @@ url = 'https://qixiangyang.cn/'
 headers = {'cookie': 'test_thread'}
 
 
-def get_data(index):
+def get_data(index, x):
     res = requests.get(url, headers=headers)
-    print(index, res.status_code)
-    time.sleep(1)
+    print('线程ID：{} 任务ID：{} 请求状态：{}'.format(index, x, res.status_code))
+    # time.sleep(1)
     return res.text
 
 
-def get_title(index, text_info):
+def get_title(index, text_info, x):
     page_dom = etree.HTML(text_info)
     title_list = page_dom.xpath('/html/body/div/div/div[1]/div/h2/a/text()')
-    print(index, title_list)
+    print('线程ID：{} 任务ID：{} 结果：{}'.format(index, x, str(title_list)))
     return title_list
 
 
-def main(index):
-    text_info = get_data(index)
-    get_title(index, text_info)
+def main(index, seed_list):
+    print(seed_list)
+    for x in seed_list:
+        text_info = get_data(index, x)
+        get_title(index, text_info, x)
 
 
+x = list(range(101, 200))
+seg = 10
 ths = []
-for _ in range(10):
-    th = threading.Thread(target=main, args=(_,))
+
+for _ in range(seg):
+    th = threading.Thread(target=main, args=(_, x[_*seg: (_+1)*seg]))
     th.start()
     ths.append(th)
 
